1eb41e29759c69531328e46d3b882620f62043e6
A.Mohan <mohan6121@email.com>
2025-06-06 15:49:14 +0530

    mohan

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 // Define GET endpoint /payment/message
-#[get("/payment")]
+#[get("/payment/message")]
 async fn payment_message() -> impl Responder {
     HttpResponse::Ok().body("Welcome to the payment service")
 }
@@ -9,13 +9,13 @@ async fn payment_message() -> impl Responder {
 // Main function to start the server
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("🚀 Starting Payment Service at http://localhost:8081");
+    println!("🚀 Starting Payment Service at http://localhost:8080");
 
     HttpServer::new(|| {
         App::new()
             .service(payment_message) // register the service
     })
-    .bind(("127.0.0.1", 8081))? // bind to localhost:8080
+    .bind(("127.0.0.1", 8080))? // bind to localhost:8080
     .run()
     .await
 }
